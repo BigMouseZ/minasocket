34c2074963b9c1b8f679e79e030a05bcc78439ce
张刚 <zhanggang@signalway.com.cn>
2018-12-28 19:03:10 +0800

    删除多余日志配置

--- police_maintenance/src/main/java/com/signalway/PolicemaintenanceApplication.java
+++ police_maintenance/src/main/java/com/signalway/PolicemaintenanceApplication.java
@@ -2,27 +2,25 @@ package com.signalway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * @author ZhangGang
+ * @author ZhangGang extends SpringBootServletInitializer
  * @createTime 2018/8/20
  **/
 @SpringBootApplication
-public class PolicemaintenanceApplication extends SpringBootServletInitializer {
+public class PolicemaintenanceApplication  {
 
 
-    @Override
+ /*   @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(PolicemaintenanceApplication.class);
-    }
+    }*/
     /**
      * @param args 入参
      * @Description 功能：
      **/
     public static void main(String[] args) {
-        SpringApplication.run(PolicemaintenanceApplication.class, args);
+        //SpringApplication.run(PolicemaintenanceApplication.class, args);
         SpringApplication springApplication = new SpringApplication(PolicemaintenanceApplication.class);
         springApplication.run(args);
     }
